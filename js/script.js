f2f9ec9c64e23b790530b4ab70bf0a2ed0280cab
Denis Vladimirov <denis@artkerr.com>
2020-03-22 21:48:44 +0300

    Анимация ошибки

--- js/script.js
+++ js/script.js
@@ -1,23 +1,60 @@
 var link = document.querySelector(".feedback-button");
+
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".close-button");
-var userName = popup.querySelector("[name=user-name]");
+
+var form = popup.querySelector(".feedback");
+var login = popup.querySelector("[name=user-name]");
+var userEmail = popup.querySelector("[name=user-email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  userName.focus();
 
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-  })
+  if (storage) {
+    userEmail.value = storage;
+    userEmail.focus();
+  } else {
+    login.focus();
+  }
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-          popup.classList.remove("modal-show");
-      }
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !userEmail.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
     }
-})
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
